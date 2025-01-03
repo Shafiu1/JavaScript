195f4dbf84d69093bc5d8d7f7264ea13fbc9863b
Shafiul Alam <148846204+Shafiu1@users.noreply.github.com>
2025-01-03 09:35:44 +0000

    variable and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail="Shafi@gmail.com"
+var accPass="12345"
+accountCity="CTG"
+let accountState
+// accountId=2
+accountEmail="rasel@gmail.com";
+console.log(accountId)
+console.table([accountEmail,accountId,accountCity,accountState])
