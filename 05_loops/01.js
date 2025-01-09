d6d2094024737b5b1eb212f360d88ad9fdd1f545
Shafiul Alam <shafiulalam0307@gmail.com>
2025-01-09 21:46:47 +0600

    add the loop folder

---
+++ 05_loops/01.js
@@ -0,0 +1,27 @@
+
+//for
+// const array=[1,2,3,4,5,6,7]
+
+// for(let index=0;index<array.length;index++){
+//     const element = array[index];
+//     console.log(element);
+// }
+
+
+// for(let i =0 ;i<=10;i++){
+//     console.log(`Outer loop value: ${i}`)
+//     for(let j=0;j<=10;j++){
+//         console.log(`       Inner loop value: ${j}`)
+//     }
+// }
+
+
+//KEYWORD : break and continue;
+
+for(let i=0;i<20;i++){
+    if(i==5)
+        continue;//break;
+    console.log(i);
+}
+
+
