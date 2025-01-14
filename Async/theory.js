9b82a2861b6780bafafef2dfeb3ec5734e48a479
Shafiul Alam <shafiulalam0307@gmail.com>
2025-01-14 19:22:05 +0600

    Async javascript

---
+++ Async/theory.js
@@ -0,0 +1,16 @@
+// JavaScript
+//     1. Synchronous execution (by default);
+//     2. Single threaded
+
+// execution context
+//     1. Execute one line of code at a time, returning
+//     2. console.log(1)
+//     3. console.log(2)
+
+// Blocking code
+//     1. block the flow of the program 
+//     2. read file synch
+
+// vs non-blocking code
+//     1. Does not block execution of code
+//     2. Read file Async
