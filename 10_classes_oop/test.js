f037743ab315fe3b84dc958244d750ea6c65bbbc
Shafiul Alam <shafiulalam0307@gmail.com>
2025-01-17 12:44:14 +0600

    testing some interview question on JS

---
+++ 10_classes_oop/test.js
@@ -0,0 +1,22 @@
+// function abc(){
+//     console.log(abc.xyz);
+// }
+// abc();
+// abc.xyz=400;
+// abc.xyz=200;
+// abc();
+
+// const arr=[1,2,3,4];
+// arr[10]=30;
+// console.log(arr);
+
+// console.log(typeof typeof 100);
+
+// const arr1 = [..."Shafiu"];
+// console.log(arr1);
+
+function test(x){
+    return x*2;
+}
+
+console.log(test(3)+test(4));
